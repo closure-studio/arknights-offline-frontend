98fb85b9fdfc18813be24077c373c2190a0b5bc9
Mix <32300164+mnixry@users.noreply.github.com>
2021-03-22 01:38:58 +0800

    :tada: finish sidebar design

---
+++ src/components/SideBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-card class="">
+    <q-img src="~assets/background.webp"
+      ><div class="text-h5 absolute-top text-center">帐号信息</div></q-img
+    >
+    <q-card-section class="shadow-1">
+      <q-list bordered>
+        <q-item
+          clickable
+          v-for="account in accounts"
+          :key="account.account"
+          :to="`/game/${account.account}`"
+          ><q-item-section avatar
+            ><q-icon
+              :name="
+                account.platform ? 'android' : 'phone_iphone'
+              " /></q-item-section
+          ><q-item-section
+            ><q-item-label>{{ account.account }}</q-item-label>
+            <q-item-label caption>
+              {{ account.platform ? 'Android' : 'iOS' }}
+            </q-item-label></q-item-section
+          >
+        </q-item>
+      </q-list>
+    </q-card-section>
+  </q-card>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import api from '../api';
+import { GameAccountData } from '../api/models';
+
+export default defineComponent({
+  props: {},
+  data: function () {
+    return {
+      accounts: [] as GameAccountData[],
+      logined: true,
+    };
+  },
+  methods: {
+    refreshAccountData: async function () {
+      try {
+        const result = await api.getGamesAccounts();
+        this.accounts = result.data;
+      } catch (err) {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.error(`Failed to refresh account data, error ${err}`);
+      }
+    },
+    checkUserLogin: async function () {
+      try {
+        const userTokenData = await api.verifyToken();
+        api.token.set(userTokenData.data.token);
+        console.info(
+          `Succeed to refresh user ${userTokenData.data.userID} JWT token`
+        );
+        this.logined = true;
+      } catch (err) {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        console.error(`Failed to refresh user token, error ${err}`);
+        this.logined = false;
+      }
+    },
+  },
+  mounted: function () {
+    setInterval(() => void this.refreshAccountData(), 10 * 1000);
+    setInterval(() => void this.checkUserLogin(), 10 * 60 * 1000);
+  },
+  setup(props) {
+    return { ...props };
+  },
+});
+</script>
