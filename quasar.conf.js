77e2b51298cf5a1135d909fff6b1f58c6818ec69
Mix <32300164+mnixry@users.noreply.github.com>
2021-03-26 21:05:53 +0800

    :globe_with_meridians: change quasar lang to zhcn

--- quasar.conf.js
+++ quasar.conf.js
@@ -79,7 +79,7 @@ module.exports = configure(function(/* ctx */) {
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      lang: 'zh-hans', // Quasar language pack
       config: {},
 
       // Possible values for "importStrategy":
