2643992c0dbfaaf3a27a6d9d5eacaa27ba14e0b3
Mix <32300164+mnixry@users.noreply.github.com>
2021-03-26 19:37:30 +0800

    :fire: remove unused code created by quasar cli

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,12 +1,3 @@
-export interface Todo {
-  id: number;
-  content: string;
-}
-
-export interface Meta {
-  totalCount: number;
-}
-
 export interface LoginFunc {
   (username: string, password: string): Promise<null>;
 }
