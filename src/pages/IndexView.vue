0dda015b7c46259e7163df207e315502f2466112
Mix <32300164+mnixry@users.noreply.github.com>
2021-04-29 17:31:20 +0800

    :lipstick: optimize index view layout

--- src/pages/IndexView.vue
+++ src/pages/IndexView.vue
@@ -6,12 +6,26 @@
       </q-card-section>
       <q-separator />
       <q-card-section>
-        <div class="row justify-center items-center">
+        <q-banner
+          title="免责声明"
+          rounded
+          class="bg-primary text-white col-12 col-md-6"
+        >
+          <template v-slot:avatar>
+            <q-icon name="warning" />
+          </template>
+          <div class="text-body1">非盈利性公益项目,且用且珍惜</div>
+          <div class="text-body1">请勿泄露您的任何账号</div>
+          <div class="text-body1">请勿肆处宣扬本网站</div>
+          <div class="text-body1">感谢您的使用</div>
+        </q-banner>
+
+        <q-scroll-area style="height: calc(40vh)">
           <q-timeline
             color="primary"
             layout="comfortable"
             v-if="!!activitiesList"
-            class="col-12 col-md-6"
+            class="q-pa-xl"
           >
             <q-timeline-entry
               v-for="activity in activitiesList"
@@ -21,21 +35,7 @@
               :body="activity.message"
             ></q-timeline-entry>
           </q-timeline>
-
-          <q-banner
-            title="免责声明"
-            rounded
-            class="bg-primary text-white col-12 col-md-6"
-          >
-            <template v-slot:avatar>
-              <q-icon name="warning" />
-            </template>
-            <div class="text-body1">非盈利性公益项目,且用且珍惜</div>
-            <div class="text-body1">请勿泄露您的任何账号</div>
-            <div class="text-body1">请勿肆处宣扬本网站</div>
-            <div class="text-body1">感谢您的使用</div>
-          </q-banner>
-        </div>
+        </q-scroll-area>
       </q-card-section>
       <q-separator />
 
